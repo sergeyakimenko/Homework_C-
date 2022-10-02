1e02300a710a49a794df8c7675d5a7e0a2a1c9f6
sergey akimenko <saguarointexas@mail.ru>
2022-10-02 20:23:08 +0300

    task_4

---
+++ task_4/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Напишите программу, которая на вход принимает число (N), 
+а на выходе показывает все чётные числа от 1 до N.
+*/
+
+Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine());
+
+if (number > 0)
+{
+    int n = 2;
+    while (n <= number)
+    {
+        Console.Write(n);
+        Console.Write(", ");
+        n = n + 2;
+    }
+}
+else 
+{   
+    Console.Write("Введите положительно число!");
+}
